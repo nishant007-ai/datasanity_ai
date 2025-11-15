3a15e6c4c98fca714b8d806baeeabaf1e627a29c
Nishant Jadhav <nnjadhav2004@gmail.com>
2025-11-15 06:42:01 +0530

    small code update

--- nis.py
+++ nis.py
@@ -1,4 +1,3 @@
  "hi": "Hello! How are you today, sir?",
     "hello": "Hi there! How can I assist you with your farming today, sir?",
-    "how are you": "I am fine! Ready to help you with your farming needs.",
- 
+  
